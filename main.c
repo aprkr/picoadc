f046cf538587d46b1ac833b2cbc7a11587efbb63
Alex <2703parker@gmail.com>
2024-04-28 17:12:41 -0500

    200 kHz rate

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 #include "tusb.h"
 
 #define BUF_SIZE 81920
+#define SAMPLING_RATE 200000
+#define CLK_DIV 48000000 / SAMPLING_RATE
 #define SHIFT true
 
 uint8_t capture_buf[BUF_SIZE];
@@ -60,7 +62,7 @@ void main() {
         false,   // We won't see the ERR bit because of 8 bit reads; disable.
         SHIFT     // Shift each sample to 8 bits when pushing to FIFO
     );
-    adc_set_clkdiv(192); // 250 kHz
+    adc_set_clkdiv(CLK_DIV);
     sleep_ms(10);
     uint dma_chan1 = dma_claim_unused_channel(true);
     dma_channel_config cfg1 = dma_channel_get_default_config(dma_chan1);
@@ -87,12 +89,6 @@ void main() {
         BUF_SIZE,  // transfer count
         true            // start immediately
     );
-    // dma_channel_configure(dma_chan2, &cfg2,
-    //     capture_buf2,    // dst
-    //     &adc_hw->fifo,  // src
-    //     BUF_SIZE,  // transfer count
-    //     false            // start immediately
-    // );
     while (tud_cdc_n_available(0) == 0) {
 
     }
@@ -119,9 +115,5 @@ void main() {
         gpio_put(LED_PIN, 0);
         dma_channel_wait_for_finish_blocking(dma_chan2);
         my_stdio_usb_out_chars(capture_buf2, BUF_SIZE);
-        
-        
-        
-        
     }
 }
